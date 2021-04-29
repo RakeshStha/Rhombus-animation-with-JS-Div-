f634835f1bf50076a5ce7e9e8e61b5d62f02bfe4
Rakesh Shrestha <rakeshshrestha3222@gmail.com>
2021-04-29 22:27:18 +0545

    Rhombus shape one rotation

--- js/script.js
+++ js/script.js
@@ -7,10 +7,10 @@ var positionY = 200;
 
 var animationX = setInterval(moveX, 10);
 
-
 function stopX(){
     clearInterval(animationX);
 }
+
 
 
 function moveX(){
@@ -28,26 +28,36 @@ function moveX(){
             positionY -=1;
             box_1.style.left = positionX + "px";
             box_1.style.top = positionY + "px";
-            if (positionY == 415){
-                clearInterval(y)
-
-                // var x = setInterval(X_Move, 10);
-                // function X_Move(){
-                //     positionX -= 1;
-                //     postionY -= 1;
-                //     box_1.style.left = positionX + "px";
-                //     box_1.style.top = positionY + "px";
-                // }
-                // console.log(positionY + positionX) 
+            if (positionY == 215){
+                clearInterval(y);
+                
+                var x = setInterval(X_Move, 10); //630
+                function X_Move(){
+                    positionX -= 1;
+                    positionY -= 1;
+                    box_1.style.left = positionX + "px";
+                    box_1.style.top = positionY + "px";
+                    if (positionX == 200){
+                        clearInterval(x);
+                        var xy = setInterval(top_Move, 10); //630
+                        function top_Move(){
+                            positionX -= 1;
+                            positionY += 1;
+                            box_1.style.left = positionX + "px";
+                            box_1.style.top = positionY + "px";
+                            if (positionX == 0){
+                                clearInterval(xy);
+                            }
+                        }
+                        
+                    }
+                }
+                
                 
                
             }
-        }
+    }
       
-
-        
-       
-
         // if (positionY == 150){
         //     clearInterval(y);
         //     // setInterval(
